57dce3706c582504466a6ea915fb097d985ddbfe
DiromaUmukoro <godbless.umukoro@yahoo.com>
2021-01-27 02:54:16 -0600

    image upload error

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -10,7 +10,7 @@ cloudinary.config({
 const storage = new CloudinaryStorage({
     cloudinary,
     params: {
-        folder: 'YelpCamp',
+        folder: 'RentYourCar',
         allowedFormats: ['jpeg', 'png', 'jpg']
     }
 });
